74a8d0627bb21b1bf59f8c7a6bc5944274a29176
zoy <zoy-l@outlook.com>
2022-08-29 15:42:15 +0800

    chore: add vue state

--- src/MState.test.ts
+++ src/MState.test.ts
@@ -48,17 +48,50 @@ describe('macro state', () => {
       `
       import { MState } from 'macro'
 
-      export default function useHook() {
-        let v = MState(false)
+      export default () => {
+        let count = MState(0)
 
-        const s = () => {
-          v = !v
+        function hello(){
+          let count = 1
+          count++
         }
 
-        return [v, s]
+        return (
+          <button onClick={() => (count += 1)}>
+            Clicked {count} {count === 1 ? 'time' : 'times'}
+          </button>
+        )
       }
     `,
-      { frame: 'react' }
+      { frame: 'vue' }
+    )
+
+    expect(code).toMatchSnapshot()
+  })
+
+  it('vue state object', () => {
+    const code = getCode(
+      `
+      import { MState } from 'macro'
+
+      export default () => {
+        const count = MState({ a:1 })
+        const foo = MState([1,2,3])
+
+        function hello(){
+          let count = 1
+          count++
+        }
+
+        return (
+          <button onClick={() => (count.a += 1)}>
+            Clicked {count.a} {count.a === 1 ? 'time' : 'times'}
+            {foo.map((i)=> <span>{i}</span>)}
+          </button>
+        )
+      }
+    `,
+      { frame: 'vue' }
     )
 
     expect(code).toMatchSnapshot()
--- src/MState.ts
+++ src/MState.ts
@@ -2,12 +2,19 @@ import * as t from '@babel/types'
 import { NodePath } from '@babel/core'
 import { addNamed } from '@babel/helper-module-imports'
 import { MParams } from './types'
+import { Scope } from '@babel/traverse'
 
 type Argument = t.Expression | t.SpreadElement | t.JSXNamespacedName | t.ArgumentPlaceholder
 
 function getInitState(path: NodePath): Argument {
   const callExpression = path.findParent((p) => t.isCallExpression(p)) as NodePath<t.CallExpression>
   return callExpression.node.arguments[0]
+}
+
+const hookCacheId = {
+  vue: null,
+  react: null,
+  soild: null
 }
 
 export function MState(path: NodePath, options: MParams) {
@@ -22,59 +29,114 @@ export function MState(path: NodePath, options: MParams) {
     .get('id') as NodePath<t.Identifier>
 
   const initState = getInitState(path)
+
   const functionDeclaration = path.getFunctionParent()!
 
   switch (opts.frame) {
     case 'react':
-      const hookId = addNamed(path, 'useState', 'react')
-      const updater = path.scope.generateUidIdentifier(`set${stateVariable.node.name}`)
+      {
+        const hookId = hookCacheId.react ?? addNamed(path, 'useState', 'react')
+        hookCacheId.react = hookId
+        const updater = path.scope.generateUidIdentifier(`set${stateVariable.node.name}`)
 
-      variableDeclaration.insertAfter(
-        t.variableDeclaration('const', [
-          t.variableDeclarator(
-            t.arrayPattern([t.identifier(stateVariable.node.name), updater]),
-            t.callExpression(hookId, [initState])
-          )
-        ])
-      )
-
-      // find assignment expression to replace
-      functionDeclaration.traverse({
-        AssignmentExpression(path: NodePath<t.AssignmentExpression>) {
-          const variable = path.get('left').node
-          if (
-            t.isIdentifier(variable) &&
-            functionDeclaration.scope.hasOwnBinding(variable.name) &&
-            variable.name == stateVariable.node.name
-          ) {
-            const stateName = functionDeclaration.scope.generateUidIdentifier(
-              stateVariable.node.name
+        variableDeclaration.insertAfter(
+          t.variableDeclaration('const', [
+            t.variableDeclarator(
+              t.arrayPattern([t.identifier(stateVariable.node.name), updater]),
+              t.callExpression(hookId, [initState])
             )
-            path.get('right').traverse({
-              Identifier(p: NodePath<t.Identifier>) {
-                if (
-                  functionDeclaration.scope.hasOwnBinding(p.node.name) &&
-                  p.node.name == stateVariable.node.name
-                ) {
-                  p.replaceWith(stateName)
+          ])
+        )
+
+        // find assignment expression to replace
+        functionDeclaration.traverse({
+          AssignmentExpression(APath: NodePath<t.AssignmentExpression>) {
+            const variable = APath.get('left').node
+            if (
+              t.isIdentifier(variable) &&
+              functionDeclaration.scope.hasOwnBinding(variable.name) &&
+              variable.name === stateVariable.node.name
+            ) {
+              const stateName = functionDeclaration.scope.generateUidIdentifier(
+                stateVariable.node.name
+              )
+              APath.get('right').traverse({
+                Identifier(p: NodePath<t.Identifier>) {
+                  if (
+                    functionDeclaration.scope.hasOwnBinding(p.node.name) &&
+                    p.node.name === stateVariable.node.name
+                  ) {
+                    p.replaceWith(stateName)
+                  }
                 }
-              }
-            })
-            path.replaceWith(
-              t.callExpression(updater, [
-                t.arrowFunctionExpression(
-                  [stateName],
-                  t.assignmentExpression(path.node.operator, stateName, path.get('right').node)
-                )
-              ])
-            )
+              })
+              APath.replaceWith(
+                t.callExpression(updater, [
+                  t.arrowFunctionExpression(
+                    [stateName],
+                    t.assignmentExpression(APath.node.operator, stateName, APath.get('right').node)
+                  )
+                ])
+              )
+            }
           }
-        }
-      })
+        })
+      }
       break
     case 'vue':
+      {
+        const hookId = hookCacheId.vue ?? addNamed(path, 'ref', 'vue')
+        hookCacheId.vue = hookId
+
+        variableDeclaration.insertAfter(
+          t.variableDeclaration('const', [
+            t.variableDeclarator(
+              t.identifier(stateVariable.node.name),
+              t.callExpression(hookId, [initState])
+            )
+          ])
+        )
+
+        const Identifiers: NodePath<t.Identifier>[] = []
+        let currentScope: Scope
+        functionDeclaration.traverse({
+          Identifier(IPath) {
+            if (
+              IPath.node.name === stateVariable.node.name &&
+              // exclude initialization nodes
+              stateVariable.node.start !== IPath.node.start &&
+              // and new replacement nodes, new node without start
+              IPath.node.start &&
+              functionDeclaration.scope.hasOwnBinding(IPath.node.name)
+            ) {
+              const IPathParent = IPath.parentPath.parentPath
+
+              // If it is an assignment expression
+              // the current scope is saved
+              // the assignment always precedes the use
+              if (t.isVariableDeclaration(IPathParent)) {
+                currentScope = IPath.scope
+              }
+
+              if (
+                !t.isVariableDeclaration(IPathParent) &&
+                // If it is in the same scope as the assignment expression, it will not be processed
+                currentScope?.block.start !== IPath.scope.block.start
+              ) {
+                Identifiers.push(IPath)
+              }
+            }
+          }
+        })
+
+        Identifiers.forEach((item) => {
+          item.replaceWith(t.memberExpression(t.identifier(item.node.name), t.identifier('value')))
+        })
+      }
       break
     case 'solid':
+      break
+    default:
       break
   }
 
