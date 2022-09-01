bfa4481f77702bf29ce5a44d47e1537487e6bdd4
zoy <zoy-l@outlook.com>
2022-09-01 14:59:09 +0800

    chore: add a type export

--- src/macro.ts
+++ src/macro.ts
@@ -7,7 +7,19 @@ import { useMount as IUseMounted } from './useMount'
 import { useMemo as IUseMemo } from './useMemo'
 import { useWatchEffect as IUseWatchEffect } from './useWatchEffect'
 
-export declare function state<T>(initialState: T): T
+export declare function state<T>(initialState: T): [T, <V extends T>(value: V) => void]
+
+export declare function component<T>(component: T): T
+
+export declare function ref<T>(initialRef: T): T
+
+export declare function proper<T>(initialProps: T): T
+
+export declare function useMount(func: Function): void
+
+export declare function useMemo<Func extends (...args: any[]) => any>(func: Func): ReturnType<Func>
+
+export declare function useWatchEffect(func: Function): void
 
 export default createMacro(({ references, state }) => {
   const idxMaps = new Set([])
